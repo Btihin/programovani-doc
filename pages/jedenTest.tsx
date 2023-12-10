9eac817ae03315f42bf33610cd3b5532ac7dadc0
Tomas Trnka <trnkatomas@outlook.com>
2023-12-10 19:34:58 +0100

    První zkušební nastavení dokumentace

---
+++ pages/jedenTest.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MyApp from '../components/counters';
+
+const JedenTest = () => {
+  return (
+    <div>
+      <h1>Jeden test</h1>
+      <div>
+        <MyApp />
+      </div>
+    </div>
+  );
+};
+
+export default JedenTest;
