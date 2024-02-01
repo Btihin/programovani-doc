f7f4df26647dddee067433553fbd2d899c30b009
Tomas Trnka <trnkatomas@outlook.com>
2024-02-01 16:26:12 +0100

    úprava převodníku

--- components/prevodnik.tsx
+++ components/prevodnik.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import styles from './prevodnik.module.css';
+import { log } from 'console';
 
 export interface IPrevodItem {
   original: string;
   mixovany: string;
   desitkovy: number;
+}
+
+type Variant = variant.ORIGINAL | variant.MIXOVANY | variant.DESITKOVY;
+export enum variant {
+  ORIGINAL = 'original',
+  MIXOVANY = 'mixovany',
+  DESITKOVY = 'desitkovy',
 }
 
 export default function Prevodnik() {
@@ -31,11 +39,14 @@ export default function Prevodnik() {
   function prohodit(item): IPrevodItem {
     console.log('item', item);
     console.log('item lenght', item.length);
-    if (item.length > 7) {
-      let jedna = item.substring(0, 2);
-      let dva = item.substring(2, 4);
-      let tri = item.substring(4, 6);
-      let ctyri = item.substring(6, 8);
+    if (item.length > 15) {
+      let bezNul = item.substring(8, item.length);
+      console.log('bez nul', bezNul);
+
+      let jedna = bezNul.substring(0, 2);
+      let dva = bezNul.substring(2, 4);
+      let tri = bezNul.substring(4, 6);
+      let ctyri = bezNul.substring(6, 8);
       let novy = ctyri + tri + dva + jedna;
       let novyDesitkovy = parseInt(novy, 16);
 
@@ -52,13 +63,39 @@ export default function Prevodnik() {
     setVysledek([]);
   };
 
-  const handleCopy = (hodnota: string) => {
+  const handleCopy = (hodnota: string[]) => {
     // let vybraneHodnoty = '';
     // if (desitky) {
     //     vybraneHodnoty = vysledek.filter()
     // } else {
     // }
-    navigator.clipboard.writeText(hodnota);
+    console.log('Kopírovaný', hodnota);
+
+    navigator.clipboard.writeText(hodnota.toString());
+  };
+
+  const ButtonCopy: FC<{ typ: Variant }> = (props) => {
+    let kopirovany = [];
+    let text = '';
+    if (props.typ === variant.ORIGINAL) {
+      kopirovany = vysledek.filter((f) => f.original);
+      text = props.typ;
+    }
+    if (props.typ === variant.MIXOVANY) {
+      kopirovany = vysledek.filter((f) => f.mixovany);
+      text = 'original';
+    }
+    if (props.typ === variant.ORIGINAL) {
+      kopirovany = vysledek.filter((f) => f.desitkovy);
+      text = 'original';
+    }
+    return (
+      <div>
+        <button className={styles.btn} onClick={() => handleCopy(kopirovany)}>
+          Kopírovat {props.typ}
+        </button>
+      </div>
+    );
   };
   return (
     <div>
@@ -90,19 +127,46 @@ export default function Prevodnik() {
             <div>Mixovany</div>
             <div>Desitkovy</div>
           </div>
-          {vysledek.map((item) => (
+          <div className={styles.divContainer}>
+            <div>
+              {vysledek.map((item) => (
+                <div className={styles.divCol}>
+                  <div>{item.original}</div>
+                </div>
+              ))}
+            </div>
+            <div>
+              {/* </div>
+          <div className={styles.divContainer}> */}
+              {vysledek.map((item) => (
+                <div className={styles.divCol}>
+                  <div>{item.mixovany}</div>
+                </div>
+              ))}
+            </div>
+            <div>
+              {/* </div>
+          <div className={styles.divContainer}> */}
+              {vysledek.map((item) => (
+                <div className={styles.divCol}>
+                  <div>{item.desitkovy}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+          {vysledek.length > 0 && (
             <div className={styles.divContainer}>
-              <div onClick={() => handleCopy(item.original)}>
-                {item.original}
+              <div>
+                <ButtonCopy typ={variant.ORIGINAL} />
               </div>
-              <div onClick={() => handleCopy(item.mixovany)}>
-                {item.mixovany}
+              <div>
+                <ButtonCopy typ={variant.MIXOVANY} />
               </div>
-              <div onClick={() => handleCopy(item.desitkovy.toString())}>
-                {item.desitkovy}
+              <div>
+                <ButtonCopy typ={variant.DESITKOVY} />
               </div>
             </div>
-          ))}
+          )}
         </div>
         <div></div>
       </div>
